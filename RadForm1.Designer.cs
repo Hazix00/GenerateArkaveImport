2f583e41e67d8043266e2ad210c21d5afcf3f93c
Taoussi Hamza <hamza92010@live.fr>
2020-01-21 10:11:26 +0100

    added progressbar

--- RadForm1.Designer.cs
+++ RadForm1.Designer.cs
@@ -50,21 +50,21 @@
             // 
             // tbServeur
             // 
-            this.tbServeur.Location = new System.Drawing.Point(147, 17);
+            this.tbServeur.Location = new System.Drawing.Point(147, 22);
             this.tbServeur.Name = "tbServeur";
             this.tbServeur.Size = new System.Drawing.Size(129, 20);
             this.tbServeur.TabIndex = 0;
             // 
             // tbLogin
             // 
-            this.tbLogin.Location = new System.Drawing.Point(147, 54);
+            this.tbLogin.Location = new System.Drawing.Point(147, 59);
             this.tbLogin.Name = "tbLogin";
             this.tbLogin.Size = new System.Drawing.Size(129, 20);
             this.tbLogin.TabIndex = 1;
             // 
             // tbPassword
             // 
-            this.tbPassword.Location = new System.Drawing.Point(147, 91);
+            this.tbPassword.Location = new System.Drawing.Point(147, 96);
             this.tbPassword.Name = "tbPassword";
             this.tbPassword.PasswordChar = '*';
             this.tbPassword.Size = new System.Drawing.Size(129, 20);
@@ -73,38 +73,38 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(22, 18);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.Size = new System.Drawing.Size(62, 16);
             this.label1.TabIndex = 3;
             this.label1.Text = "Serveur";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(22, 55);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 60);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.Size = new System.Drawing.Size(46, 16);
             this.label2.TabIndex = 4;
             this.label2.Text = "Login";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(22, 92);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 97);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 17);
+            this.label3.Size = new System.Drawing.Size(80, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "Password ";
             // 
             // btnConnexion
             // 
-            this.btnConnexion.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnexion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnConnexion.ForeColor = System.Drawing.Color.Black;
-            this.btnConnexion.Location = new System.Drawing.Point(363, 116);
+            this.btnConnexion.Location = new System.Drawing.Point(363, 121);
             this.btnConnexion.Name = "btnConnexion";
             this.btnConnexion.Size = new System.Drawing.Size(121, 33);
             this.btnConnexion.TabIndex = 6;
@@ -115,9 +115,9 @@
             // bntGenerer
             // 
             this.bntGenerer.Enabled = false;
-            this.bntGenerer.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntGenerer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bntGenerer.ForeColor = System.Drawing.Color.Black;
-            this.bntGenerer.Location = new System.Drawing.Point(375, 303);
+            this.bntGenerer.Location = new System.Drawing.Point(401, 303);
             this.bntGenerer.Name = "bntGenerer";
             this.bntGenerer.Size = new System.Drawing.Size(121, 33);
             this.bntGenerer.TabIndex = 7;
@@ -127,9 +127,9 @@
             // 
             // btnAnnuler
             // 
-            this.btnAnnuler.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnnuler.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAnnuler.ForeColor = System.Drawing.Color.Black;
-            this.btnAnnuler.Location = new System.Drawing.Point(30, 303);
+            this.btnAnnuler.Location = new System.Drawing.Point(12, 303);
             this.btnAnnuler.Name = "btnAnnuler";
             this.btnAnnuler.Size = new System.Drawing.Size(121, 33);
             this.btnAnnuler.TabIndex = 8;
@@ -139,17 +139,17 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(22, 129);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 134);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 17);
+            this.label4.Size = new System.Drawing.Size(65, 16);
             this.label4.TabIndex = 12;
             this.label4.Text = "Tranche";
             // 
             // comboxTranche
             // 
             this.comboxTranche.FormattingEnabled = true;
-            this.comboxTranche.Location = new System.Drawing.Point(147, 128);
+            this.comboxTranche.Location = new System.Drawing.Point(147, 133);
             this.comboxTranche.Name = "comboxTranche";
             this.comboxTranche.Size = new System.Drawing.Size(129, 21);
             this.comboxTranche.TabIndex = 11;
@@ -174,10 +174,12 @@
             // 
             // radProgressBar1
             // 
-            this.radProgressBar1.Location = new System.Drawing.Point(30, 227);
+            this.radProgressBar1.Location = new System.Drawing.Point(12, 227);
             this.radProgressBar1.Name = "radProgressBar1";
-            this.radProgressBar1.Size = new System.Drawing.Size(466, 32);
+            this.radProgressBar1.Size = new System.Drawing.Size(510, 32);
+            this.radProgressBar1.Step = 1;
             this.radProgressBar1.TabIndex = 14;
+            this.radProgressBar1.Text = "0%";
             // 
             // radLabel1
             // 
